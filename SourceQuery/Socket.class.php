803fbf784d42466f1d9c28426a670e16e51d890a
Kubo2 <kelerest123@gmail.com>
2015-01-12 22:23:12 +0100

    SourceQuerySocket: Minor cosmetic changes
     - initial work on phpDoc
     - add typehint to constructor
     - reorder very first assignments in SourceQuerySocket::Open()
     - replace parameters with clearer use of $this->* variables assigned
       sooner

--- SourceQuery/Socket.class.php
+++ SourceQuery/Socket.class.php
@@ -22,7 +22,7 @@
 		 */
 		private $Buffer;
 		
-		public function __construct( $Buffer )
+		public function __construct( SourceQueryBuffer $Buffer )
 		{
 			$this->Buffer = $Buffer;
 		}
@@ -37,26 +37,45 @@
 			}
 		}
 		
-		public function Open( $Ip, $Port, $Timeout, $Engine )
+		/**
+		 * Opens a socket connection to the server specified by an $ip address
+		 * on the specified $port with specific $timeout and an $engine-specific
+		 * behavior.
+		 *
+		 * @param string $ip 		A valid IPv4 address of the server
+		 * @param int $port 			Port to use when opening connection
+		 * @param int $timeout 		A time period (in seconds) for a server to accept connection
+		 * @param int $engine 		An engine protocol to use for a communication (either 
+		 *                     			SourceQuery::GOLDSOURCE or SourceQuery::SOURCE constant)
+		 * @return boolean 			always TRUE
+		 * @throws \Exception 		if it is not possible to connect
+		 */
+		public function Open( $ip, $port, $timeout, $engine )
 		{
-			$this->Timeout = $Timeout;
-			$this->Engine  = $Engine;
-			$this->Port    = $Port;
-			$this->Ip      = $Ip;
+			// internal connection attributes
+			$this->Ip = $ip;
+			$this->Port = $port;
+			$this->Timeout = $timeout;
+			$this->Engine = $engine;
 			
-			$this->Socket = @FSockOpen( 'udp://' . $Ip, $Port, $ErrNo, $ErrStr, $Timeout );
+			// socket held for a next session
+			$this->Socket = @FSockOpen( "udp://{$this->Ip}", $this->Port, $ErrNo, $ErrStr, $this->Timeout );
 			
+			// socket could not be opened
 			if( $ErrNo || $this->Socket === false )
 			{
 				throw new Exception( 'Could not create socket: ' . $ErrStr );
 			}
 			
-			Stream_Set_Timeout( $this->Socket, $Timeout );
+			Stream_Set_Timeout( $this->Socket, $this->Timeout );
 			Stream_Set_Blocking( $this->Socket, true );
 			
 			return true;
 		}
 		
+		/**
+		 * @return boolean whether sending the data to a server succeeded
+		 */
 		public function Write( $Header, $String = '' )
 		{
 			$Command = Pack( 'ccccca*', 0xFF, 0xFF, 0xFF, 0xFF, $Header, $String );
@@ -65,6 +84,9 @@
 			return $Length === FWrite( $this->Socket, $Command, $Length );
 		}
 		
+		/**
+		 * @throws SourceQueryException
+		 */
 		public function Read( $Length = 1400 )
 		{
 			$this->Buffer->Set( FRead( $this->Socket, $Length ) );
@@ -155,6 +177,9 @@
 			}
 		}
 		
+		/**
+		 * @return boolean
+		 */
 		private function Sherlock( $Length )
 		{
 			$Data = FRead( $this->Socket, $Length );
